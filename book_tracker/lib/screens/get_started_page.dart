cfba857cf5a29ac2972fb5aa37123aad32f6b49c
pdichone <pdichone@gmail.com>
2021-05-04 14:26:37 -0700

    Get Started page finished

---
+++ book_tracker/lib/screens/get_started_page.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class GetStartedPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      child: CircleAvatar(
+        backgroundColor: HexColor('#f5f6f8'),
+        child: Column(children: [
+          Spacer(),
+          Text(
+            'BookTracker',
+            style: Theme.of(context).textTheme.headline3,
+          ),
+          SizedBox(
+            height: 15,
+          ),
+          Text(' "Read. Change. Yourself"',
+              style: TextStyle(
+                  color: Colors.black26,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 29)),
+          SizedBox(
+            height: 50,
+          ),
+          TextButton.icon(
+              style: TextButton.styleFrom(
+                  primary: Colors.white,
+                  backgroundColor: HexColor('#69639f'),
+                  textStyle: TextStyle(fontSize: 18)),
+              onPressed: () {},
+              icon: Icon(Icons.login_rounded),
+              label: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('Sign in to get started'),
+              )),
+          Spacer()
+        ]),
+      ),
+    );
+  }
+}
