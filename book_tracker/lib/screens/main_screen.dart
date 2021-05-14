5aa3ff804f09e494aa21126452f16c143dcfe55e
pdichone <pdichone@gmail.com>
2021-05-14 07:28:41 -0700

    update main screen

--- book_tracker/lib/screens/main_screen.dart
+++ book_tracker/lib/screens/main_screen.dart
@@ -1,15 +1,10 @@
-import 'dart:html';
-
-import 'package:book_tracker/model/book.dart';
 import 'package:book_tracker/model/user.dart';
 import 'package:book_tracker/screens/login_page.dart';
 import 'package:book_tracker/widgets/create_profile.dart';
-import 'package:book_tracker/widgets/create_profile_mobile.dart';
 import 'package:book_tracker/widgets/input_decoration.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:hexcolor/hexcolor.dart';
 
 class MainScreenPage extends StatelessWidget {
   @override
@@ -99,6 +94,54 @@ class MainScreenPage extends StatelessWidget {
               icon: Icon(Icons.logout),
               label: Text(''))
         ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => BookSearchPage(),
+              ));
+        },
+        child: Icon(Icons.add),
+        backgroundColor: Colors.redAccent,
+      ),
+    );
+  }
+}
+
+class BookSearchPage extends StatelessWidget {
+  final TextEditingController _searchTextController = TextEditingController();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Book Search'),
+        backgroundColor: Colors.redAccent,
+      ),
+      body: Material(
+        elevation: 0.0,
+        child: Center(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width * 0.9,
+            height: MediaQuery.of(context).size.height * 0.9,
+            child: Column(
+              children: [
+                Container(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Form(
+                        child: TextField(
+                      controller: _searchTextController,
+                      decoration: buildInputDecoration(
+                          label: 'Search', hintText: 'Flutter Development'),
+                    )),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
